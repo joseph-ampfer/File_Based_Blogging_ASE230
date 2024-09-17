198fec60322d0e20a468c66d2e0278deca8a63bd
Joseph Ampfer <joseph.ampfer@gmail.com>
2024-09-16 21:33:44 -0400

    Get post_id in detail.php

---
+++ Project/detail.php
@@ -0,0 +1,39 @@
+<?php
+$posts = [
+  [
+    'title' => 'Title 1',
+    'content' => '',
+    'author' => '',
+    'date' => ''
+  ],
+  [
+    'title' => 'Title 2',
+    'content' => '',
+    'author' => '',
+    'date' => ''
+  ],
+  [
+    'title' => 'Title 3',
+    'content' => '',
+    'author' => '',
+    'date' => ''
+  ],
+  [
+    'title' => 'Title 4',
+    'content' => '',
+    'author' => '',
+    'date' => ''
+  ],
+
+];
+
+$post_id = $_GET['post_id'];
+
+echo $post_id;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <title></title>
+  <body></body>
+</html>
